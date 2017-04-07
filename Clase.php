6602faa783623d45c12d59ba801528b496f2cc8b
jjrp267 <jjrp267@gmail.com>
2017-04-07 14:44:18 +0200

    Create Clase.php

---
+++ Clase.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   AkeebaBackup
+ * @copyright Copyright (c)2009-2014 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ *
+ * @since     4.4.0
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+use Akeeba\Engine\Platform;
+use Akeeba\Engine\Factory;
+
+class Clase
+{
+	/** @var   JSession   The session we're working with */
+	protected $session = null;
+  
+}
